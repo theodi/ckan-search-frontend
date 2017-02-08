5fe35c5a454416d359443b85bd71de7dea9aa542
James Smith <james@floppy.org.uk>
2017-02-08 15:17:06 +0000

    Add index page

--- search.rb
+++ search.rb
@@ -4,6 +4,10 @@ require 'yaml'
 
 CKAN::API.api_url = "https://data.gov.uk/api/2/"
 
+get '/' do
+  erb :index
+end
+
 get '/:query' do
   @packages = CKAN::Package.find(q: params['query'])
   erb :results
