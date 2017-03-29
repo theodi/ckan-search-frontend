b921d509ec896341234991cc9d1518af3c4ab88b
James Smith <james@floppy.org.uk>
2017-03-29 11:50:56 +0100

    add entities and grouping

--- search.rb
+++ search.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'ckan'
 require 'yaml'
+require 'csv'
 
 CKAN::API.api_url = "https://data.gov.uk/api/2/"
 
@@ -10,6 +11,18 @@ end
 
 get '/:query' do
   @packages = CKAN::Package.find(q: params['query'])
+  
+  @all_entities = @packages.map do |package| 
+    extra_data = YAML.load_file("data/#{package.name}.yml") rescue {}
+    extra_data["entities"] || []
+  end
+  @all_entities.flatten!
+  
+  @entity_groups = {}
+  @all_entities.uniq.each do |e|
+    @entity_groups[e] = @all_entities.count(e)
+  end
+  
   erb :results
 end
   
